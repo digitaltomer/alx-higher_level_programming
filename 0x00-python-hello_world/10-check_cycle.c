c550806bc1f4f769454828dc9d6f7db09cef3d86
digitaltomer <tomamadin@gmail.com>
2022-12-05 12:50:29 -0800

    changes

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,27 +1,30 @@
+#include <stdlib.h>
 #include "lists.h"
 
 /**
- * check_cycle - checks if a linked list is circular
- * @list: linked list to check
+ * check_cycle - Checks if a singly-linked list contains a cycle.
+ * @list: A singly-linked list.
  *
- * Return: 1 if the list has a cycle
- * 0 if none
+ * Return: If there is no cycle - 0.
+ *         If there is a cycle - 1.
  */
-
 int check_cycle(listint_t *list)
 {
-	listint_t *red = list;
-	listint_t *blue = list;
+	listint_t *turtle, *hare;
 
-	if (!list)
+	if (list == NULL || list->next == NULL)
 		return (0);
 
-	while (red && blue && blue->next)
+	turtle = list->next;
+	hare = list->next->next;
+
+	while (turtle && hare && hare->next)
 	{
-		red = red->next;
-		blue = blue->next->next;
-		if (red == blue)
+		if (turtle == hare)
 			return (1);
+
+		turtle = turtle->next;
+		hare = hare->next->next;
 	}
 
 	return (0);
