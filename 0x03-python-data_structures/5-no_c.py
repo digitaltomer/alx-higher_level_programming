e431742f5d1e64b719c2558975aed6a94136dc3a
digitaltomer <tomamadin@gmail.com>
2022-12-05 16:07:30 -0800

    no copy

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    new_str = ""
+    for a in my_string:
+        if a == "c" or a == "C":
+            continue
+        else:
+            new_str += a
+    return new_str
