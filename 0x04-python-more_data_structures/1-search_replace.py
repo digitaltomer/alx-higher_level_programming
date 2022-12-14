e811580d8c270c923f6ae1f03331e4c0efb27bf3
digitaltomer <tomamadin@gmail.com>
2022-12-13 23:39:24 -0800

    search fix

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-def uniq_add(my_list=[]):
-    uniq = [*set(my_list)]
-    u_sum = 0
-
-    for i in uniq:
-        u_sum += i
-    return (u_sum)
-# plist = [1, 2, 3, 4, 2, 1, 5, 3]
-# print(uniq_add(plist))
+def search_replace(my_list, search, replace):
+    new_list = my_list[:]
+    for i in range(len(new_list)):
+        if new_list[i] == search:
+            new_list[i] = replace
+    return(new_list)
