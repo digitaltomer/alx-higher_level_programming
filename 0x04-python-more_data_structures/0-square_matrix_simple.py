b569eeaf7b7f81e431b70cc56e4cf39c3d06c718
digitaltomer <tomamadin@gmail.com>
2022-12-13 23:29:27 -0800

    matrix fix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    matrix_set = list[map(lambda x: x ** 2, lst)) for lst in matrix]
-    return (matrix_set)
+    return ([list(map(lambda x: x * x, row)) for row in matrix])
