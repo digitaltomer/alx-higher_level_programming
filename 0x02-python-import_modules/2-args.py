5480ac21d61c0377a373daf2efb37d6dc21615a2
digitaltomer <tomamadin@gmail.com>
2022-12-15 05:57:41 -0800

    argv fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,12 +2,16 @@
 if __name__ == "__main__":
     import sys
 
-    count = len(sys.argv) - 1
-    if count == 0:
-        print("0 argument.")
-    elif count == 1:
-        print("1 argument:")
+    num_of_args = len(sys.argv) - 1
+
+    if num_of_args == 0:
+        print("{} arguments.".format(num_of_args))
+    elif num_of_args == 1:
+        print("{} argument:".format(num_of_args))
+        print("{}: {}".format(num_of_args, sys.argv[1]))
     else:
-        print("{} arguments:".format(count))
-    for k in range(count):
-        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+        print("{} arguments:".format(num_of_args))
+        # loop through the list and print
+        for i in range(num_of_args + 1):
+            if i != 0:
+                print("{}: {}".format(i, sys.argv[i]))
