37d2538d2932205b1990657df7b17a15bc1a9151
Digitaltomer <tomamadin@gmail.com>
2022-12-21 23:53:39 +0100

    list fix

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,8 +5,8 @@ def safe_print_list(my_list=[], x=0):
         try:
             print(f"{my_list[i]}", end="")
             i += 1
-         except IndexError:
-             break
+        except IndexError:
+            break
 
      print()
      return(i)
