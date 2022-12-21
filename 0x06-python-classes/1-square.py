f40ab6400666c7a685223c0c9a9dd3e02a12f02c
digitaltomer <tomamadin@gmail.com>
2022-12-20 16:44:57 -0800

    sqpy

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines a Square"""
+
+
+class Square:
+    """Represents a Square"""
+
+    def __init__(self, size):
+        """Initializing this square class
+        Args: size - represnets the size of the square defined
+        """
+
+        self.__size = size
