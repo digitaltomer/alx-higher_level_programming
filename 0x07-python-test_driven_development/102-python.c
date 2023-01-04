d3f6ea2c9eba152ffc2860894b79128af73c3934
Digitaltomer <tomamadin@gmail.com>
2023-01-04 10:15:51 +0100

    fix

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -25,4 +25,5 @@ void print_python_string(PyObject *p)
 	else
 		printf("  type: compact unicode object\n");
 	printf("  length: %ld\n", length);
-	
+	printf("  value: %ls\n", PyUnicode_AsWideCharString(p, &length));
+}
