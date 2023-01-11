549c306f0ada93c9a5c25a3f3ddcd8d842886c88
digitaltomer <tomamadin@gmail.com>
2023-01-11 14:11:55 -0800

    write fix

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,7 +3,6 @@
 
 
 def append_write(filename="", text=""):
-    """Appends a string to the end of a UTF8 text file
-    """
+    """Appends a string to the end of a UTF8 text file"""
     with open(filename, "a", encoding="utf-8") as f:
         return f.write(text)
