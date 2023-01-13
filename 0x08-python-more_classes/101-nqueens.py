a53a42d0ea8978d83a38614d82a9afa5d233298f
digitaltomer <tomamadin@gmail.com>
2023-01-13 06:45:24 -0800

    thir fix

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -104,7 +104,7 @@ def recursive_solve(board, row, queens, solution):
             solutions = recursive_solve(tmp_board, row + 1,
                                         queens + 1, solutions)
 
-    return
+    return (solutions)
 
 
 if __name__ = "__main__":
@@ -113,6 +113,9 @@ if __name__ = "__main__":
         sys.exit(1)
     if sys.argv[1].isdigit() is False:
         print("N must be a number")
+        sys.exit(1)
+    if int(sys.argv[1]) < 4:
+        print("N must be at least 4")
         sys.exit(1)
 
     board = init_board(int(sys.argv[1]))
